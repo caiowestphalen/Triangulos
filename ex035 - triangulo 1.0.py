29e9fd41c1cc70e5f6aae395daf5d76f1c4c2db4
caiowestphalen <caiowestphalen@hotmail.com>
2022-04-22 10:40:59 -0300

    Update ex035 - triangulo 1.0.py

--- ex035 - triangulo 1.0.py
+++ ex035 - triangulo 1.0.py
@@ -1,4 +1,5 @@
-#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+# Programa que te diz baseando em três medidas se forma um triangulo ou não.
+
 from time import sleep
 print('-='*20)
 print('Analisando um triangulo')
@@ -12,13 +13,15 @@ if a < b + c and b < a + c and c < a + b: #// codigo da aula
      print('Os segmentos acima PODEM FORMAR um triângulo!')
 else:
      print('Os segmentos NÃO PODEM FORMAR!')
+     
+//////////////////////////////////////////////////////////////////////////////////////
 
-# testando se é triangulo // codigo v2
-'''if (a + b < c) or (a + c < b) or (b + c < a):
+# Versão V2 - informa se é EQUILATERO, ISOSCELES ou ESCALENO
+if (a + b < c) or (a + c < b) or (b + c < a):
     print('Esse segmento NÃO FORMA um triangulo')
 elif (a == b) and (a == c):
     print('Esse segmento é um EQUILATERO.')
 elif (a == b) or (a == c) or (b == c):
     print('Esse segmento é um ISÓSCELES.')
 else:
-    print('Esse segmento é um ESCALENO.')'''
+    print('Esse segmento é um ESCALENO.')
